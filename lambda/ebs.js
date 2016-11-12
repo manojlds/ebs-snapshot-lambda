4c8a3515a7b25b8ceea19479e7d9f64a8d7dcc1c
Manoj <manojlds@gmail.com>
2016-11-12 17:56:57 +0530

    Refactoring to make it more readable

--- lambda/ebs.js
+++ lambda/ebs.js
@@ -5,6 +5,44 @@ var AWS = require('aws-sdk');
 var ec2 = new AWS.EC2(utils.getRegionObject());
 var config = require('./config.json');
 
+var getPurgeDate = function(tags) {
+  var purgeDate = new Date();
+  purgeDate.setTime(purgeDate.getTime() + (tags['Retention'] || config.defaultRetention) * 86400000 );
+  
+  return purgeDate;
+};
+
+var createSnapshot = function(volumeId) {
+  var snapshotParams = {
+    VolumeId: volumeId,
+    DryRun: false
+  };
+  
+  return ec2.createSnapshot(snapshotParams).promise();
+};
+
+var tagSnapshot = function(volume, snapshotId) {
+  var tags = utils.getTags(volume.Tags);
+  var purgeDate = getPurgeDate(tags);
+
+  var snapshotTagParams = {
+    Resources: [snapshotId],
+    Tags: [
+      {
+        Key: 'VolumeId',
+        Value: volume.VolumeId
+      },
+      {
+        Key: 'PurgeDate',
+        Value: purgeDate.toISOString().split('T')[0]
+      },
+    ],
+    DryRun: false
+  };
+  
+  return ec2.createTags(snapshotTagParams).promise();
+}
+
 var snapshotVolumes = function () {
   var getVolumesParam = {
     DryRun: false,
@@ -22,38 +60,10 @@ var snapshotVolumes = function () {
     .promise()
     .then(function(data) {
       return data.Volumes.map(function(volume) {
-        var tags = volume.Tags.reduce(function(final, current) {
-          final[current.Key] = current.Value;
-          return final;
-        }, {});
-        var purgeDate = new Date();
-        purgeDate.setTime( purgeDate.getTime() + (tags['Retention'] || config.defaultRetention) * 86400000 );
         
-        var snapshotParams = {
-          VolumeId: volume.VolumeId,
-          DryRun: false
-        };
-        var snapshotTagParams = {
-          Resources: [
-          ],
-          Tags: [
-            {
-              Key: 'VolumeId',
-              Value: volume.VolumeId
-            },
-            {
-              Key: 'PurgeDate',
-              Value: purgeDate.toISOString().split('T')[0]
-            },
-          ],
-          DryRun: false
-        };
-        
-        return ec2.createSnapshot(snapshotParams)
-          .promise()
+        return createSnapshot(volume.VolumeId)
           .then(function(data) {
-            snapshotTagParams.Resources = [data.SnapshotId];
-            return ec2.createTags(snapshotTagParams).promise();
+            return tagSnapshot(volume, data.SnapshotId);
           })
       });
     });
--- lambda/utils.js
+++ lambda/utils.js
@@ -8,5 +8,13 @@ var getRegionObject = function() {
   return { region: region };
 }
 
+var getTags = function(tags) {
+  return tags.reduce(function(final, current) {
+    final[current.Key] = current.Value;
+    return final;
+  }, {});
+}
+
 exports.getRegion = getRegion;
 exports.getRegionObject = getRegionObject;
+exports.getTags = getTags;
