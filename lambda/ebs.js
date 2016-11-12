04dbc9b61a5df5726911d778b745d2e48e6267f1
Manoj <manojlds@gmail.com>
2016-11-12 17:27:46 +0530

    Create and tag snapshots

--- lambda/ebs.js
+++ lambda/ebs.js
@@ -3,21 +3,62 @@ var utils = require('./utils');
 var Promise = require('bluebird');
 var AWS = require('aws-sdk');
 var ec2 = new AWS.EC2(utils.getRegionObject());
+var config = require('./config.json');
 
-var snapshotVolumes = function*() {
+var snapshotVolumes = function () {
   var getVolumesParam = {
     DryRun: false,
     Filters: [
       {
-        Name: "tag-keys",
+        Name: "tag-key",
         Values: [
           "Snapshot"
         ]
       },
-    ],
-    MaxResults: 10,
+    ]
   };
   
-  var volumes = yield ec2.describeVolumes(getVolumesParam).promise();
-  console.log(volumes);
+  var snapshotPromises = ec2.describeVolumes(getVolumesParam)
+    .promise()
+    .then(function(data) {
+      return data.Volumes.map(function(volume) {
+        var tags = volume.Tags.reduce(function(final, current) {
+          final[current.Key] = current.Value;
+          return final;
+        }, {});
+        var purgeDate = new Date();
+        purgeDate.setTime( purgeDate.getTime() + (tags['Retention'] || config.defaultRetention) * 86400000 );
+        
+        var snapshotParams = {
+          VolumeId: volume.VolumeId,
+          DryRun: false
+        };
+        var snapshotTagParams = {
+          Resources: [
+          ],
+          Tags: [
+            {
+              Key: 'VolumeId',
+              Value: volume.VolumeId
+            },
+            {
+              Key: 'PurgeDate',
+              Value: purgeDate.toISOString().split('T')[0]
+            },
+          ],
+          DryRun: false
+        };
+        
+        return ec2.createSnapshot(snapshotParams)
+          .promise()
+          .then(function(data) {
+            snapshotTagParams.Resources = [data.SnapshotId];
+            return ec2.createTags(snapshotTagParams).promise();
+          })
+      });
+    });
+    
+    return Promise.all(snapshotPromises);
 };
+
+exports.snapshotVolumes = snapshotVolumes;
--- lambda/ebs_snapshot_lambda.js
+++ lambda/ebs_snapshot_lambda.js
@@ -1,6 +1,6 @@
 var Promise = require('bluebird');
 
-var ebs = require('./ebs.js');
+var ebs = require('./ebs');
 
 var handler = function* (event, context) {
   try {
@@ -18,5 +18,5 @@ exports.handler = Promise.coroutine(handler);
 //Uncomment below to test locally
 exports.handler(null, {
   done: function(e, s) { console.log(s); },
-  error: function(e, ex) { console.log(ex);}
+  error: function(e, ex) { console.log(ex); }
 });
