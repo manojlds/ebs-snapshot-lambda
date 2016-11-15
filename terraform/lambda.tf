c3d98af65375c1a19ec65c80068943c3c0afe9a0
Manoj <manojlds@gmail.com>
2016-11-15 14:58:51 +0530

    Making the lambda schedule name unique

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "ebs_snapshot_lambda" {
 }
 
 resource "aws_cloudwatch_event_rule" "lambda_schedule" {
-  name = "lambda_schedule"
+  name = "lambda_schedule_ebs_snapshot_lambda"
   description = "Lambda Schedule"
   schedule_expression = "rate(${var.lamba_schedue})"
 }
