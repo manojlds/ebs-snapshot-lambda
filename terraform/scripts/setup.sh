3ea098ef89fc7246bec2f24e93f9f09f53b47565
Manoj <manojlds@gmail.com>
2016-11-15 12:58:10 +0530

    Minor changes in setup.sh

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LAMBDA_DIR="$1"
+LAMBDA_PREPARED_DIR="$2"
 
 pushd "${LAMBDA_DIR}"
 rm -rf ebs_snapshot_lambda-*.tgz
 npm pack
 
-pushd "$2"
+pushd "${LAMBDA_PREPARED_DIR}"
 rm -rf package/
 tar xvf ${LAMBDA_DIR}/ebs_snapshot_lambda-*.tgz
+
 pushd package/
 npm install --production
